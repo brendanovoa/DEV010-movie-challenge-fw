bccee1b91417628198e0111ae97ea9c6fcd092e5
Brenda Hernandez <brendanovoabn@gmail.com>
2023-11-24 19:17:44 -0600

    Filters component with html and styles

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { FiltersComponent } from './views/filters/filters.component';
+import { MovieDetailsComponent } from './views/movie-details/movie-details.component';
+import { MoviesComponent } from './views/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'filters', component: FiltersComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'movieDetails', component: MovieDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- movie-app/src/app/app.module.ts
+++ movie-app/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './views/home/home.component';
+import { ButtonComponent } from './components/button/button.component';
+import { FiltersComponent } from './views/filters/filters.component';
+import { MoviesComponent } from './views/movies/movies.component';
+import { MovieDetailsComponent } from './views/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    HomeComponent,
+    FiltersComponent,
+    MoviesComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
